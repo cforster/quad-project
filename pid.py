9ccbc7321df2e5185108eb21d1eaa7fd9b9b3890
Matt Gruskin <mattgruskin@google.com>
2013-08-07 23:46:59 -0400

    implement a python pid controller - we'll probably need it

---
+++ pid.py
@@ -0,0 +1,66 @@
+import logging
+import time
+
+logger = logging.getLogger('pid')
+
+class PID(object):
+  def __init__(self, kp=0.5, ki=0.2, kd=0.75,
+               integ_max=1000.0, out_max=100, out_min=0):
+    self._kp = kp
+    self._ki = ki
+    self._kd = kd
+    self._integ_max = integ_max
+    self._out_max = out_max
+    self._out_min = out_min
+
+    self._setpoint = 0
+    self._last_error = 0.0
+    self._integral = 0.0
+    self._last_update_time = time.time()
+
+  def SetSetpoint(self, setpoint):
+    self._setpoint = setpoint
+
+  def Update(self, measured):
+    now = time.time()
+    dt = now - self._last_update_time
+    self._last_update_time = now
+
+    error = self._setpoint - measured
+
+    self._integral += error * dt
+    if self._integral > self._integ_max:
+      self._integral = self._integ_max
+    elif self._integral < -self._integ_max:
+      self._integral = -self._integ_max
+
+    derivative = (error - self._last_error) / dt
+
+    p = self._kp * error
+    i = self._ki * self._integral
+    d = self._kd * derivative
+    logger.debug('p=%5f i=%5f d=%5f', p, i, d)
+
+    output = p + i + d
+    if output > self._out_max:
+      output = self._out_max
+    elif output < self._out_min:
+      output = self._out_min
+
+    self._last_error = error
+    return output
+
+if __name__ == '__main__':
+  # test the PID controller
+  import random
+  logging.basicConfig(level=logging.DEBUG)
+  p = PID()
+  p.SetSetpoint(100)
+  pos = 50
+  velocity = 1
+  while True:
+    output = p.Update(pos)
+    logger.debug('pos=%5f vel=%5f out=%5f', pos, velocity, output)
+    pos += velocity
+    velocity += output * 0.02 + random.uniform(-0.1, 0.1) - 0.5
+    time.sleep(0.1)
